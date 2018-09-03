d7f7a7ca1aecd2941df310f7a15ea8a4e3b6294c
Yami Iglesia <miriamiglesia@gmail.com>
2018-09-03 15:46:21 +0900

    feature: add env and load in server

--- server.go
+++ server.go
@@ -1,18 +1,32 @@
 package main
 
 import(
+	"os"
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/contrib/static"
 
 	handler "vewlog/handlers"
 )
 
+func init() {
+	// Load ENV file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
 	router := gin.Default()
 
-	handler.SetupRoutes(router)
 	// Serve Static Files
 	router.Use(static.Serve("/", static.LocalFile("./views/build", true)))
 
-	router.Run(":8088")
+	handler.SetupRoutes(router)
+
+	port := os.Getenv("PORT")
+	router.Run(":" + port)
 }
